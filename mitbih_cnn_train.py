70072c040f12318e435bd02baf3ad8c107038786
Anson Antony <ansonanto97@gmail.com>
2021-08-07 20:17:53 +0530

    Add files via upload

---
+++ mitbih_cnn_train.py
@@ -0,0 +1,281 @@
+import math
+import random
+import pickle
+import itertools
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, label_ranking_average_precision_score, label_ranking_loss, coverage_error 
+
+from sklearn.utils import shuffle
+
+from scipy.signal import resample
+
+import matplotlib.pyplot as plt
+
+np.random.seed(42)
+
+import pickle
+from sklearn.preprocessing import OneHotEncoder
+from keras.models import Model
+from keras.layers import Input, Dense, Conv1D, MaxPooling1D, Softmax, Add, Flatten, Activation# , Dropout
+from keras import backend as K
+from keras.optimizers import Adam
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+import pdb; 
+import os
+
+colNames=['f1','f2','f3','f4','f5','f6','f7','f8','f9','f10','f11','f12','f13','f14','f15','f16','f17','f18','f19','f20','f21','f22','f23','f24','f25','f26','f27','f28','f29','f30','f31','f32','f33','f34','f35','f36','f37','f38','f39','f40','f41','f42','f43','f44','f45','f46','f47','f48','f49','f50','f51','f52','f53','f54','f55','f56','f57','f58','f59','f60','f61','f62','f63','f64','f65','f66','f67','f68','f69','f70','f71','f72','f73','f74','f75','f76','f77','f78','f79','f80','f81','f82','f83','f84','f85','f86','f87','f88','f89','f90','f91','f92','f93','f94','f95','f96','f97','f98','f99','f100','f101','f102','f103','f104','f105','f106','f107','f108','f109','f110','f111','f112','f113','f114','f115','f116','f117','f118','f119','f120','f121','f122','f123','f124','f125','f126','f127','f128','f129','f130','f131','f132','f133','f134','f135','f136','f137','f138','f139','f140','f141','f142','f143','f144','f145','f146','f147','f148','f149','f150','f151','f152','f153','f154','f155','f156','f157','f158','f159','f160','f161','f162','f163','f164','f165','f166','f167','f168','f169','f170','f171','f172','f173','f174','f175','f176','f177','f178','f179','f180','f181','f182','f183','f184','f185','f186','f187','decision']
+
+train_path="mitbih_train.csv"
+test_path="mitbih_test.csv"
+
+def stretch(x):
+    l = int(187 * (1 + (random.random()-0.5)/3))
+    y = resample(x, l)
+    if l < 187:
+        y_ = np.zeros(shape=(187, ))
+        y_[:l] = y
+    else:
+        y_ = y[:187]
+    return y_
+
+def amplify(x):
+    alpha = (random.random()-0.5)
+    factor = -alpha*x + (1+alpha)
+    return x*factor
+
+def augment(x):
+    result = np.zeros(shape= (4, 187))
+    for i in range(3):
+        if random.random() < 0.33:
+            new_y = stretch(x)
+        elif random.random() < 0.66:
+            new_y = amplify(x)
+        else:
+            new_y = stretch(x)
+            new_y = amplify(new_y)
+        result[i, :] = new_y
+    return result
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+df = pd.read_csv(train_path, header=None, skiprows = [0])
+df2 = pd.read_csv(test_path, header=None, skiprows = [0])#header=0,names=colNames)
+df = pd.concat([df, df2], axis=0)
+df.head()
+df.info()
+df[187].value_counts()
+M = df.values
+X = M[:, :-1]
+y = M[:, -1].astype(int)
+del df
+del df2
+del M
+C0 = np.argwhere(y == 0).flatten()
+C1 = np.argwhere(y == 1).flatten()
+C2 = np.argwhere(y == 2).flatten()
+C3 = np.argwhere(y == 3).flatten()
+C4 = np.argwhere(y == 4).flatten()
+
+x = np.arange(0, 187)*8/1000
+
+plt.figure(figsize=(20,12))
+plt.plot(x, X[C0, :][0], label="Cat. N")
+plt.plot(x, X[C1, :][0], label="Cat. S")
+plt.plot(x, X[C2, :][0], label="Cat. V")
+plt.plot(x, X[C3, :][0], label="Cat. F")
+plt.plot(x, X[C4, :][0], label="Cat. Q")
+plt.legend()
+plt.title("1-beat ECG for every category", fontsize=20)
+plt.ylabel("Amplitude", fontsize=15)
+plt.xlabel("Time (ms)", fontsize=15)
+plt.show()
+plt.plot(X[0, :])
+plt.plot(amplify(X[0, :]))
+plt.plot(stretch(X[0, :]))
+plt.show()
+
+result = np.apply_along_axis(augment, axis=1, arr=X[C3]).reshape(-1, 187)
+classe = np.ones(shape=(result.shape[0],), dtype=int)*3
+X = np.vstack([X, result])
+y = np.hstack([y, classe])
+
+subC0 = np.random.choice(C0, 800)
+subC1 = np.random.choice(C1, 800)
+subC2 = np.random.choice(C2, 800)
+subC3 = np.random.choice(C3, 800)
+subC4 = np.random.choice(C4, 800)
+#pdb.set_trace()
+X_test = np.vstack([X[subC0], X[subC1], X[subC2], X[subC3], X[subC4]])
+y_test = np.hstack([y[subC0], y[subC1], y[subC2], y[subC3], y[subC4]])
+
+X_train = np.delete(X, [subC0, subC1, subC2, subC3, subC4], axis=0)
+y_train = np.delete(y, [subC0, subC1, subC2, subC3, subC4], axis=0)
+
+X_train, y_train = shuffle(X_train, y_train, random_state=0)
+X_test, y_test = shuffle(X_test, y_test, random_state=0)
+
+del X
+del y
+
+
+X_train = np.expand_dims(X_train, 2)
+X_test = np.expand_dims(X_test, 2)
+
+
+print("X_train", X_train.shape)
+print("y_train", y_train.shape)
+print("X_test", X_test.shape)
+print("y_test", y_test.shape)
+
+
+ohe = OneHotEncoder()
+y_train = ohe.fit_transform(y_train.reshape(-1,1))
+y_test = ohe.transform(y_test.reshape(-1,1))
+
+
+
+print("X_train", X_train.shape)
+print("y_train", y_train.shape)
+print("X_test", X_test.shape)
+print("y_test", y_test.shape)
+
+n_obs, feature, depth = X_train.shape
+batch_size = 500
+
+K.clear_session()
+
+inp = Input(shape=(feature, depth))
+C = Conv1D(filters=32, kernel_size=5, strides=1)(inp)
+C11 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(C)
+A11 = Activation("relu")(C11)
+C12 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(A11)
+S11 = Add()([C12, C])
+A12 = Activation("relu")(S11)
+M11 = MaxPooling1D(pool_size=5, strides=2)(A12)
+
+
+C21 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(M11)
+A21 = Activation("relu")(C21)
+C22 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(A21)
+S21 = Add()([C22, M11])
+A22 = Activation("relu")(S11)
+M21 = MaxPooling1D(pool_size=5, strides=2)(A22)
+
+
+C31 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(M21)
+A31 = Activation("relu")(C31)
+C32 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(A31)
+S31 = Add()([C32, M21])
+A32 = Activation("relu")(S31)
+M31 = MaxPooling1D(pool_size=5, strides=2)(A32)
+
+
+C41 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(M31)
+A41 = Activation("relu")(C41)
+C42 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(A41)
+S41 = Add()([C42, M31])
+A42 = Activation("relu")(S41)
+M41 = MaxPooling1D(pool_size=5, strides=2)(A42)
+
+
+C51 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(M41)
+A51 = Activation("relu")(C51)
+C52 = Conv1D(filters=32, kernel_size=5, strides=1, padding='same')(A51)
+S51 = Add()([C52, M41])
+A52 = Activation("relu")(S51)
+M51 = MaxPooling1D(pool_size=5, strides=2)(A52)
+
+F1 = Flatten()(M51)
+
+D1 = Dense(32)(F1)
+A6 = Activation("relu")(D1)
+D2 = Dense(32)(A6)
+D3 = Dense(5)(D2)
+A7 = Softmax()(D3)
+
+model = Model(inputs=inp, outputs=A7)
+model.summary()
+def exp_decay(epoch):
+    initial_lrate = 0.001
+    k = 0.75
+    t = n_obs//(10000 * batch_size)  # every epoch we do n_obs/batch_size iteration
+    lrate = initial_lrate * math.exp(-k*t)
+    return lrate
+
+lrate = LearningRateScheduler(exp_decay)
+
+
+
+adam = Adam(lr = 0.001, beta_1 = 0.9, beta_2 = 0.999)
+
+
+model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+history = model.fit(X_train, y_train, 
+                    epochs=1, 
+                    batch_size=batch_size, 
+                    verbose=2, 
+                    validation_data=(X_test, y_test), 
+                    callbacks=[lrate])
+
+scores = model.evaluate(X_train, y_train, verbose=0)
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+y_pred = model.predict(X_test, batch_size=1000)
+# serialize model to JSON
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to disk")
+print(classification_report(y_test.argmax(axis=1), y_pred.argmax(axis=1)))
+print("ranking-based average precision : {:.3f}".format(label_ranking_average_precision_score(y_test.todense(), y_pred)))
+print("Ranking loss : {:.3f}".format(label_ranking_loss(y_test.todense(), y_pred)))
+print("Coverage_error : {:.3f}".format(coverage_error(y_test.todense(), y_pred)))
+
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1))
+np.set_printoptions(precision=2)
+
+# Plot non-normalized confusion matrix
+plt.figure(figsize=(10, 10))
+plot_confusion_matrix(cnf_matrix, classes=['N', 'S', 'V', 'F', 'Q'],
+                      title='Confusion matrix, without normalization')
+plt.show()
+
+
